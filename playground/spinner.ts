2f2d1d7d9dfc50ea2bb5bc545f25d862e2430b15
Ivo Dolenc <ivodolenc@gmail.com>
2023-10-15 11:41:42 +0200

    chore: update playground

--- playground/spinner.ts
+++ playground/spinner.ts
@@ -2,20 +2,36 @@ import { createSpinner } from '../src/index.js'
 
 const spinner = createSpinner()
 
-spinner.start()
+spinner.start({
+  message: 'Step 1: Loading...',
+})
 
 setTimeout(() => {
   spinner.update({
-    message: 'Still loading...',
+    message: 'Step 1: Still loading...',
   })
 }, 1000)
 
 setTimeout(() => {
   spinner.update({
-    message: 'Almost done...',
+    message: 'Step 1: Almost done...',
   })
 }, 2000)
 
 setTimeout(() => {
-  spinner.stop()
+  spinner.stop({
+    message: 'Step 1: Done!',
+  })
 }, 3000)
+
+setTimeout(() => {
+  spinner.start({
+    message: 'Step 2: Loading...',
+  })
+}, 4000)
+
+setTimeout(() => {
+  spinner.stop({
+    message: 'Step 2: Done!',
+  })
+}, 5000)
